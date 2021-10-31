41089569b972849b9d889ed79374ec717990ec89
encisoda <encisoda@gmail.com>
2021-10-31 12:23:21 -0300

    Se modifico el style se agregaron icon y se la library typed.js

--- docs/js/main.js
+++ docs/js/main.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
     /* ======= Chart ========= */
 
     $('.chart').easyPieChart({
-		barColor:'#00BCD4',//Pie chart colour
+		barColor:'#f1ad1a',//Pie chart colour
 		trackColor: '#e8e8e8',
 		scaleColor: false,
 		lineWidth : 5,
@@ -73,6 +73,24 @@ $(document).ready(function() {
           $typeGroup.find('.active').removeClass('active');
           $( this ).addClass('active');
         });
+    });
+
+    /* ======= Write Effect ======= */
+
+    const typed = new Typed('.typed-title',{
+        strings: ['Welcome','I am Developer', 'I am Programmer'],
+        stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
+        typeSpeed: 75, // Velocidad en mlisegundos para poner una letra,
+        startDelay: 300, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
+        backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
+        smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
+        shuffle: false, // Alterar el orden en el que escribe las palabras.
+        backDelay: 1500, // Tiempo de espera despues de que termina de escribir una palabra.
+        loop: true, // Repetir el array de strings
+        loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
+        showCursor: true, // Mostrar cursor palpitanto
+        cursorChar: '|', // Caracter para el cursor
+        contentType: 'html', // 'html' o 'null' para texto sin formato
     });
 
 
