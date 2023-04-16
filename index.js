f0244ef88d9cfcca63080cae8c87a5899d6f2ee1
TMkXNh8cX <denciso@sspv.com.ar>
2023-04-16 14:36:06 -0300

    Modificacion del html y se elimino la carpeta de docs de la version 1

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
-function showabout(){
-    $("#about_container").css("display","inherit");
-    $("#about_container").addClass("animated slideInLeft");
+function showeducation(){
+    $("#education_container").css("display","inherit");
+    $("#education_container").addClass("animated slideInLeft");
     setTimeout(function(){
-        $("#about_container").removeClass("animated slideInLeft");
+        $("#education_container").removeClass("animated slideInLeft");
     },800);
 }
-function closeabout(){
-    $("#about_container").addClass("animated slideOutLeft");
+function closeeducation(){
+    $("#education_container").addClass("animated slideOutLeft");
     setTimeout(function(){
-        $("#about_container").removeClass("animated slideOutLeft");
-        $("#about_container").css("display","none");
+        $("#education_container").removeClass("animated slideOutLeft");
+        $("#education_container").css("display","none");
     },800);
 }
-function showwork(){
-    $("#work_container").css("display","inherit");
-    $("#work_container").addClass("animated slideInRight");
+function showworkexperiences(){
+    $("#workexperiences_container").css("display","inherit");
+    $("#workexperiences_container").addClass("animated slideInRight");
     setTimeout(function(){
-        $("#work_container").removeClass("animated slideInRight");
+        $("#workexperiences_container").removeClass("animated slideInRight");
     },800);
 }
-function closework(){
-    $("#work_container").addClass("animated slideOutRight");
+function closeworkexperiences(){
+    $("#workexperiences_container").addClass("animated slideOutRight");
     setTimeout(function(){
-        $("#work_container").removeClass("animated slideOutRight");
-        $("#work_container").css("display","none");
+        $("#workexperiences_container").removeClass("animated slideOutRight");
+        $("#workexperiences_container").css("display","none");
     },800);
 }
-function showcontact(){
-    $("#contact_container").css("display","inherit");
-    $("#contact_container").addClass("animated slideInUp");
+function showskills(){
+    $("#skills_container").css("display","inherit");
+    $("#skills_container").addClass("animated slideInUp");
     setTimeout(function(){
-        $("#contact_container").removeClass("animated slideInUp");
+        $("#skills_container").removeClass("animated slideInUp");
     },800);
 }
-function closecontact(){
-    $("#contact_container").addClass("animated slideOutDown");
+function closeskills(){
+    $("#skills_container").addClass("animated slideOutDown");
     setTimeout(function(){
-        $("#contact_container").removeClass("animated slideOutDown");
-        $("#contact_container").css("display","none");
+        $("#skills_container").removeClass("animated slideOutDown");
+        $("#skills_container").css("display","none");
     },800);
 }
 setTimeout(function(){
@@ -47,7 +47,8 @@ setTimeout(function(){
       $("#loading").css("display","none");
       $("#box").css("display","none");
       $("#about").removeClass("animated fadeIn");
-      $("#contact").removeClass("animated fadeIn");
+      $("#skills").removeClass("animated fadeIn");
       $("#work").removeClass("animated fadeIn");
     },1000);
 },1500);
+
